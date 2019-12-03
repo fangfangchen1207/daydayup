b761405cf7dd05574cc31c9691cc8a4034cbf28e
fangfangchen1207 <fangfang.chen@holla.world>
2019-12-03 14:54:12 +0800

    多行下划线

---
+++ daydayup/多行下划线/HWUnderLineLabel.h
@@ -0,0 +1,36 @@
+//
+//  HWUnderLineLabel.h
+//  HOLLA
+//
+//  Created by 陈方方 on 2019/11/28.
+//  Copyright © 2019 HOLLA. All rights reserved.
+// 支持多行自定义下划线颜色和高度的label
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HWUnderLineLabel : UILabel
+
+/// 行间距
+@property (nonatomic, assign) float lineSpace;
+
+/// 下划线高度
+@property (nonatomic, assign) float underLineHeight;
+
+/// 下划线颜色
+@property (nonatomic, strong) UIColor *underLineColor;
+
+///快速创建；
+/// @param frame frame
+/// @param text text
+/// @param font font
+/// @param textColor textColor
+/// @param underLineHeight 下划线高度
+/// @param underLineColor 下划线颜色
+/// @param lineSpace 行间距
++ (HWUnderLineLabel *)createLabelWithFrame:(CGRect)frame contentText:(NSString *)text font:(UIFont *)font textColor:(UIColor *)textColor underLineHeight:(float)underLineHeight underLineColor:(UIColor *)underLineColor lineSpace:(float)lineSpace;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ daydayup/多行下划线/HWUnderLineLabel.m
@@ -0,0 +1,90 @@
+//
+//  HWUnderLineLabel.m
+//  HOLLA
+//
+//  Created by 陈方方 on 2019/11/28.
+//  Copyright © 2019 HOLLA. All rights reserved.
+//
+
+#import "HWUnderLineLabel.h"
+#import <CoreText/CoreText.h>
+#import "Default.h"
+
+@implementation HWUnderLineLabel
+
+
++(HWUnderLineLabel *)createLabelWithFrame:(CGRect)frame contentText:(NSString *)text font:(UIFont *)font textColor:(UIColor *)textColor underLineHeight:(float)underLineHeight underLineColor:(UIColor *)underLineColor lineSpace:(float)lineSpace
+{
+	HWUnderLineLabel *desLabel = [[HWUnderLineLabel alloc] initWithFrame:frame];
+	desLabel.textColor = textColor;
+	desLabel.numberOfLines = 0;
+	desLabel.textAlignment = NSTextAlignmentNatural;
+	desLabel.lineBreakMode = NSLineBreakByWordWrapping;
+	
+	NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+	paragraphStyle.lineSpacing = lineSpace;  //设置行间距
+	paragraphStyle.lineBreakMode = desLabel.lineBreakMode;
+	paragraphStyle.alignment = desLabel.textAlignment;
+	paragraphStyle.alignment = Arabic ?NSTextAlignmentRight :desLabel.textAlignment;
+
+	NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:text];
+	[attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, [text length])];
+	[attributedString addAttribute:NSFontAttributeName value:font range:NSMakeRange(0, [text length])];
+
+	desLabel.attributedText = attributedString;
+	desLabel.lineSpace =lineSpace;
+	desLabel.underLineHeight = underLineHeight;
+	desLabel.underLineColor = underLineColor;
+	[desLabel sizeToFit];
+	return desLabel;
+}
+- (instancetype)initWithFrame:(CGRect)frame{
+	if (self = [super initWithFrame:frame]) {
+		self.font = [UIFont systemFontOfSize:20];
+		self.underLineHeight = 1;
+		self.underLineColor = [UIColor blackColor];
+
+	}
+	return self;
+}
+
+- (void)drawRect:(CGRect)rect{
+	CGContextRef ctx = UIGraphicsGetCurrentContext();
+	CGContextSetTextMatrix(ctx , CGAffineTransformIdentity);
+	//x，y轴方向移动
+	CGContextTranslateCTM(ctx , 0 ,self.bounds.size.height);
+	//缩放x，y轴方向缩放，－1.0为反向1.0倍,坐标系转换,沿x轴翻转180度
+	CGContextScaleCTM(ctx, 1.0 ,-1.0);
+
+	CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)self.attributedText);
+	CGMutablePathRef Path = CGPathCreateMutable();
+	//坐标点在左下角
+	CGPathAddRect(Path, NULL ,CGRectMake(0 ,0 ,self.bounds.size.width, self.bounds.size.height));
+	CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), Path, NULL);
+	CFArrayRef Lines = CTFrameGetLines(frame);
+	CFIndex linecount = CFArrayGetCount(Lines);
+	CGPoint origins[linecount];
+	CTFrameGetLineOrigins(frame,CFRangeMake(0, 0), origins);
+	NSInteger lineIndex = 0;
+	NSArray *linesArray = (__bridge NSArray *)Lines;
+	
+	//设置颜色
+	CGContextSetStrokeColorWithColor(ctx, self.underLineColor.CGColor);
+	for (id oneLine in linesArray)
+	{
+		CGRect lineBounds = CTLineGetImageBounds((CTLineRef)oneLine, ctx);
+		lineBounds.origin.x += origins[lineIndex].x;
+		lineBounds.origin.y += origins[lineIndex].y;
+		lineIndex++;
+		//设置线宽
+		CGContextSetLineWidth(ctx, 10.0);
+		CGContextMoveToPoint(ctx, lineBounds.origin.x, lineBounds.origin.y);
+		CGContextAddLineToPoint(ctx, lineBounds.origin.x+lineBounds.size.width, lineBounds.origin.y);
+		CGContextStrokePath(ctx);
+	}
+
+	CTFrameDraw(frame,ctx);
+	CGPathRelease(Path);
+	CFRelease(framesetter);
+}
+@end
